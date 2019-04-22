3d17c02778d1f0253475d811c9506c2dff6fa16d
jacek.chowaniec <jacek.chowaniec85@gmail.com>
2019-04-22 21:34:13 +0200

    add LoadableComponent

--- src/main/java/com/petclinic/pages/HomePage.java
+++ src/main/java/com/petclinic/pages/HomePage.java
@@ -1,4 +1,20 @@
 package com.petclinic.pages;
 
-public class HomePage {
+import org.openqa.selenium.support.ui.LoadableComponent;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.url;
+import static org.fest.assertions.Assertions.assertThat;
+
+public class HomePage extends LoadableComponent<HomePage> {
+
+    @Override
+    protected void load() {
+        open("http://localhost:8080/");
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+        assertThat(url()).isEqualTo("http://localhost:8080/");
+    }
 }
