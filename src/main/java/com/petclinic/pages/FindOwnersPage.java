8dcfa09480d185b0927446fef35ef8101e282288
jacek.chowaniec <jacek.chowaniec85@gmail.com>
2019-04-25 21:21:36 +0200

    shouldDisplayListOfAllOwners test

--- src/main/java/com/petclinic/pages/FindOwnersPage.java
+++ src/main/java/com/petclinic/pages/FindOwnersPage.java
@@ -2,13 +2,23 @@ package com.petclinic.pages;
 
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.LoadableComponent;
 
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.WebDriverRunner.url;
+import static org.fest.assertions.Assertions.assertThat;
 
-public class FindOwnersPage {
+public class FindOwnersPage extends LoadableComponent<FindOwnersPage> {
 
     @FindBy(css = "div[class^='container'] h2")
     private SelenideElement pageTitle;
+
+    @FindBy(css = "button[type='submit']")
+    private SelenideElement findOwnerButton;
+
+    @FindBy(css = "table[id='vets']")
+    private SelenideElement ownersList;
 
     public FindOwnersPage() {
         page(this);
@@ -16,6 +26,29 @@ public class FindOwnersPage {
 
     public String getPageTitle() {
         return pageTitle.getText();
+    }
+
+    public FindOwnersPage clickFindOwnerButton() {
+        findOwnerButton.click();
+        return this;
+    }
+
+    public boolean isOwnersListDisplayed() {
+        return ownersList.isDisplayed();
+    }
+
+    @Override
+    protected void load() {
+        open("http://localhost:8080/owners/find");
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+        try {
+            assertThat(url()).isEqualTo("http://localhost:8080/owners/find");
+        } catch (IllegalStateException e) {
+            throw new Error(e);
+        }
     }
 
 }
--- src/test/java/com/petclinic/FindOwnersPetTest.java
+++ src/test/java/com/petclinic/FindOwnersPetTest.java
@@ -1,6 +1,24 @@
 package com.petclinic;
 
-public class FindOwnersPetTest {
+import com.petclinic.driver.SeleniumBase;
+import com.petclinic.pages.FindOwnersPage;
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class FindOwnersPetTest extends SeleniumBase {
+
+    @Test
+    public void shouldDisplayListOfAllOwners() {
+        // given
+        FindOwnersPage findOwnersPage = new FindOwnersPage().get();
+
+        // when
+        findOwnersPage.clickFindOwnerButton();
+
+        // then
+        assertThat(findOwnersPage.isOwnersListDisplayed()).isTrue();
+    }
 
 
 }
