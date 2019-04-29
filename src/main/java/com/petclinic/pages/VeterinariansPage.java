c7ae4853afb14e922b56a176a64f0082d0ccc471
jacek.chowaniec <jacek.chowaniec85@gmail.com>
2019-04-29 20:55:50 +0200

    VeterinariansTest

--- src/main/java/com/petclinic/pages/VeterinariansPage.java
+++ src/main/java/com/petclinic/pages/VeterinariansPage.java
@@ -1,14 +1,24 @@
 package com.petclinic.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import com.netflix.config.DynamicProperty;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.LoadableComponent;
 
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.WebDriverRunner.url;
+import static org.fest.assertions.Assertions.assertThat;
 
-public class VeterinariansPage {
+public class VeterinariansPage extends LoadableComponent<VeterinariansPage> {
 
     @FindBy(css = "div[class^='container'] h2")
     private SelenideElement pageTitle;
+
+    @FindBy(id = "vets")
+    private SelenideElement veterinariansTable;
+
+    private String veterinariansPageUrl = DynamicProperty.getInstance("petclinic.url").getString().concat("vets.html");
 
     public VeterinariansPage() {
         page(this);
@@ -16,5 +26,23 @@ public class VeterinariansPage {
 
     public String getPageTitle() {
         return pageTitle.getText();
+    }
+
+    public boolean isVeterinariansTableDisplayed() {
+        return veterinariansTable.isDisplayed();
+    }
+
+    @Override
+    protected void load() {
+        open(veterinariansPageUrl);
+    }
+
+    @Override
+    protected void isLoaded() throws Error {
+        try {
+            assertThat(url()).isEqualTo(veterinariansPageUrl);
+        } catch (IllegalStateException e) {
+            throw new Error(e);
+        }
     }
 }
---
+++ src/test/java/com/petclinic/VeterinariansTest.java
@@ -0,0 +1,19 @@
+package com.petclinic;
+
+import com.petclinic.driver.SeleniumBase;
+import com.petclinic.pages.VeterinariansPage;
+import org.testng.annotations.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class VeterinariansTest extends SeleniumBase {
+
+    @Test
+    public void shouldDisplayVeterinariansTable() {
+        // when
+        VeterinariansPage veterinariansPage = new VeterinariansPage().get();
+
+        // then
+        assertThat(veterinariansPage.isVeterinariansTableDisplayed()).isTrue();
+    }
+}
